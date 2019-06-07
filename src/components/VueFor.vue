8fa4b6948d551e98fbb46a971ab54025decbce39
Kenny Urena <urenakenny@gmail.com>
2019-06-07 19:15:19 -0400

    Looping through objects, properties of objects, and extracting their keys

--- src/components/VueFor.vue
+++ src/components/VueFor.vue
@@ -5,6 +5,19 @@
         {{ ingredient }} ({{ i }})
       </li>
     </ul>
+    <br />
+    <ul>
+      <li v-for="person in persons" v-bind:key="person.name">
+        {{ person.name }}
+        <br />
+        <h2>
+          <span v-for="(value, key, index) in person" v-bind:key="value">
+            {{ key }} --> {{ value }} at {{ index }},
+          </span>
+        </h2>
+      </li>
+    </ul>
+    <br />
     <template v-for="(ingredient, index) in ingredients">
       <h1 v-bind:key="ingredient">{{ ingredient }}</h1>
       <p v-bind:key="index">{{ index }}</p>
