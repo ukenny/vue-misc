0074d908fa47766e26dfd10f617cb0b7f732cfd9
Kenny Urena <urenakenny@gmail.com>
2019-06-07 18:27:33 -0400

    Added an additional component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld />
+    <VueFor />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import VueFor from "./components/VueFor.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    VueFor
   }
 };
 </script>
---
+++ src/components/VueFor.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="vue-for">
+    Hello from VueFor!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueFor"
+};
+</script>
+
+<style></style>
